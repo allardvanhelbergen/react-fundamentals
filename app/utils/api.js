9890e29eca439c6c8a2004b9563872e08dd288a7
Allard van Helbergen <allard@vanhelbergen.com>
2017-09-23 17:19:47 +1000

    Add api functions to calculate score.

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,6 +1,77 @@
 const axios = require('axios');
 
+// Only use this if you get rate limited
+const CLIENT_ID = 'client id';
+const CLIENT_SECRET = 'client secret';
+let params = '?client_id' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET;
+params = null;
+
+
+function getProfile(username) {
+  return axios.get('https://api.github.com/users/' + username + params)
+    .then(function(user) {
+      return user.data;
+    });
+}
+
+
+function getRepos(username) {
+  return axios.get('https://api.github.com/users/' + username +
+      '/repos' + params + '&per_page=100')
+}
+
+
+function getStarCount(repos) {
+  return repos.data.reduce(function(count, repo) {
+    return count + repo.stargazers_count;
+  }, 0);
+}
+
+
+function calculateScore(profile, repos) {
+  let followers = profile.followers;
+  let totalStars = getStarCount(repos);
+
+  return (3 * followers) + totalStars;
+}
+
+
+function handleError(error) {
+  console.warn(error);
+  return null;
+}
+
+
+function getUserData(player) {
+
+  return axios.all([
+    getProfile(player),
+    getRepos(player)
+  ]).then(function(data) {
+    let profile = data[0];
+    let repos = data[1];
+
+    return {
+      profile: profile,
+      score: calculateScore(profile, repos)
+    };
+  })
+}
+
+
+function sortPlayers(players) {
+  return players.sort(function(a, b) {
+    return b.score - a.score;
+  })
+}
+
 module.exports = {
+  battle: function(players) {
+    return axios.all(players.map(getUserData))
+      .then(sortPlayers)
+      .catch(handleError);
+  },
+
   fetchPopularRepos: function(language) {
     var encodedURI = window.encodeURI(
         'https://api.github.com/search/repositories?q=stars:>1+language:' +
